fc6a433ce914bb7a9ee5ae94057ceed5a41e6480
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-22 00:20:40 -0300

    created firsts js element selectors

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const markAllRead = document.querySelector('.notifications__mark__all');
+
+const unreadNotifications = document.querySelectorAll(
+  '.notification__container .unread'
+);
+
+const notificationContentUnread = document.querySelectorAll('.notification__content.unread')
+
+// const markNotificationRead = window.getComputedStyle(notificationContentUnread,':after')
