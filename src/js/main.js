048c05e01aa9540af2936c417fb1a02cb7005ab7
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-23 16:42:34 -0300

    changed functions and constants position in main js

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,11 @@
-const allNotifications = document.querySelectorAll('.notification__container')
+const allNotifications = document.querySelectorAll('.notification__container');
 
 allNotifications.forEach((item, index) => {
   if (index < 3) {
-    item.classList.add('unread')
-    item.children[1].classList.add('unread')
+    item.classList.add('unread');
+    item.children[1].classList.add('unread');
   }
-})
-
-const markAllRead = document.querySelector('.notifications__mark__all');
-
-const unreadNotifications = document.querySelectorAll(
-  '.notification__container .unread'
-);
-
-const unreadCounter = document.querySelector('.notifications__counter');
+});
 
 const unreadNotificationContent = document.querySelectorAll(
   '.notification__content.unread'
@@ -25,16 +17,22 @@ unreadNotificationContent.forEach((item) => {
     .classList.add('notification__mark__read');
 });
 
-const markRead = document.querySelectorAll('.notification__mark__read');
+const markAllRead = document.querySelector('.notifications__mark__all');
+
+const unreadNotifications = document.querySelectorAll(
+  '.notification__container .unread'
+);
+
+const unreadCounter = document.querySelector('.notifications__counter');
 
 unreadCounter.children[0].innerHTML = unreadNotifications.length;
 
-function handleMarkUnread(event) {
+function handleMarkRead(event) {
   event.target.parentElement.classList.remove('unread');
   event.target.parentElement.parentElement.classList.remove('unread');
   +unreadCounter.children[0].innerHTML--;
 }
 
-markRead.forEach((button) =>
-  button.addEventListener('click', handleMarkUnread)
-);
+const markRead = document.querySelectorAll('.notification__mark__read');
+
+markRead.forEach((button) => button.addEventListener('click', handleMarkRead));
