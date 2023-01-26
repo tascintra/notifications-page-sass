a92555f1bfab8ac8e62f9394d9cedfdeb03d5f3d
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-26 14:02:18 -0300

    mark read buttons media query interaction created

--- src/js/modules/unreadButton.js
+++ src/js/modules/unreadButton.js
@@ -3,9 +3,26 @@ export default function handleUnreadButton() {
     '.notification__content.unread'
   );
 
+  const mediaQuery = window.matchMedia('(max-width: 700px)');
+
   unreadNotificationContent.forEach((item) => {
     item
       .appendChild(document.createElement('button'))
       .classList.add('notification__mark__read');
+
+    item.parentElement
+      .appendChild(document.createElement('button'))
+      .classList.add('notification__mark__read');
+
+    function handleScreenResize(event) {
+      !event.matches
+        ? ((item.parentElement.lastChild.style.display = 'none'),
+          (item.lastChild.style.display = 'block'))
+        : ((item.parentElement.lastChild.style.display = 'block'),
+          (item.lastChild.style.display = 'none'));
+    }
+
+    mediaQuery.addListener(handleScreenResize);
+    handleScreenResize(mediaQuery);
   });
 }
