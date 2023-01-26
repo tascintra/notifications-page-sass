cafe4299000f03f4ac6bb559b48c5acf2b7b16d1
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-26 14:37:47 -0300

    mark read buttons media query interaction fixed

--- src/js/modules/markRead.js
+++ src/js/modules/markRead.js
@@ -4,6 +4,8 @@ export default function markRead() {
   function handleMarkRead(event) {
     event.target.parentElement.classList.remove('unread');
     event.target.parentElement.parentElement.classList.remove('unread');
+    event.target.parentElement.lastChild.classList.remove('unread');
+    event.target.previousElementSibling.classList.remove('unread');
     unreadCounter.children[0].innerHTML--;
   }
 
--- src/js/modules/unreadButton.js
+++ src/js/modules/unreadButton.js
@@ -3,8 +3,6 @@ export default function handleUnreadButton() {
     '.notification__content.unread'
   );
 
-  const mediaQuery = window.matchMedia('(max-width: 700px)');
-
   unreadNotificationContent.forEach((item) => {
     item
       .appendChild(document.createElement('button'))
@@ -13,16 +11,5 @@ export default function handleUnreadButton() {
     item.parentElement
       .appendChild(document.createElement('button'))
       .classList.add('notification__mark__read');
-
-    function handleScreenResize(event) {
-      !event.matches
-        ? ((item.parentElement.lastChild.style.display = 'none'),
-          (item.lastChild.style.display = 'block'))
-        : ((item.parentElement.lastChild.style.display = 'block'),
-          (item.lastChild.style.display = 'none'));
-    }
-
-    mediaQuery.addListener(handleScreenResize);
-    handleScreenResize(mediaQuery);
   });
 }
